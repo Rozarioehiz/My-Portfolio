62d6d1ecdb6bff837e1545b1843171612876ab31
Rozarioehiz <rozarioehiz@gmail.com>
2024-03-17 13:27:13 -0700

    first commit

---
+++ portfoilo/src/App.jsx
@@ -0,0 +1,13 @@
+  import React from 'react'
+import Body from './assets/component/Body'
+  
+  const App = () => {
+    return (
+      <div>
+        <Body/>
+      </div>
+    )
+  }
+  
+  export default App
+  
---
+++ portfoilo/src/assets/component/Body.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Body.css'
+
+const Body = () => {
+  return (
+<>
+<div className='General-Body'>
+    <div className="Left-General-Body">
+         <div className="Name-Stack-Done">
+             <div className="Name"><h1>Adoyi Rose Ehi</h1></div>
+                <div className="Stack"><h2>Frontend Developer</h2></div>
+                     <div className="Done"><p>
+                    What is Lorem Ipsum?  Lorem Ipsum is simply dummy text of the printing</p></div>
+            </div>
+                     <div className="About-Experience-Project"></div>
+                          <div className="ContactUs"></div>
+</div>
+                              <div className="Right-General-Body"></div>
+</div>
+</>
+  )
+}
+
+export default Body
